7bbbe8a199f95929cbb379962f40b2987fc6bea3
BIT <parksomi31>
2024-03-06 18:34:06 +0900

    merge

---
+++ src/test2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class test2 {
+	 public static void main(String []args) {
+		 Scanner sc = new Scanner(System.in) {
+			 
+			 public static double num1 {
+				 
+			 }
+			 System.out.println("숫자를 입력하세요");
+			 int a = sc.nextlnt();
+			 
+			 System.out.println
+		 }
